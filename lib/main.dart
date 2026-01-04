b64290613afe7400d8e93b3896a6743ddc3785b4
ThankGod Cyril Uche <thankgoduchecyril@gmail.com>
2026-01-04 23:35:09 +0100

    style: Add comment to closing bracket in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -83,7 +83,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
-          ],
+          ], //
         ),
       ),
       floatingActionButton: FloatingActionButton(
